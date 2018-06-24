eba2d33c5c80bccb60517eab6831ee1e46fcb98b
Junior Claudio <juniorclaudiozavaleta@gmail.com>
2018-06-23 23:28:17 -0500

    tweets sentiment finished -> 80%

--- tweet_sentiment.py
+++ tweet_sentiment.py
@@ -7,7 +7,7 @@ excel = pd.ExcelFile('./tweets_clasificados.xlsx')
 df = excel.parse('tweets')
 tokenizer = TweetTokenizer(strip_handles=True, reduce_len=True, preserve_case=False)
 
-some_words = ['cada', 'asi', 'años', 'aún', 'aunque', 'pues', 'hace']
+some_words = ['cada', 'asi', 'años', 'aún', 'aunque', 'pues', 'hace', 'tras']
 
 
 def word_tokenize(sentence):
@@ -23,15 +23,12 @@ for index, row in df.iterrows():
     df.tweet[index] = ' '.join(cleaned_words)
     all_words = all_words + [word for word in cleaned_words]
 freq = nltk.FreqDist(all_words)
-all_words = list(freq.most_common())[:300]
+# tup[0]: word, tup[1]: frequency
+all_words = [tup[0] for tup in freq.most_common()][:400]
 
 t = [({word: (word in row['tweet'].split(' ')) for word in all_words}, row['clase']) for i, row in df.iterrows()]
-# for word in all_words:
-#     for i, row in df.iterrows():
-#         t.append(({word: (word in row['tweet'].split(' '))}, row['clase']))
 
-print(len(t))
-train_set, test_set = t[2363:], t[:2363]
+train_set, test_set = t[:4000], t[4000:]
 classifier = nltk.NaiveBayesClassifier.train(train_set)
-print(nltk.classify.accuracy(classifier, test_set))
+print('Precision: ', nltk.classify.accuracy(classifier, test_set))
 classifier.show_most_informative_features()
